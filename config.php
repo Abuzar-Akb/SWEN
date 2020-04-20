2d8faa98e5650fbc74b3c0bf76ab5ef79aeea9c5
Abuzar <abuzar_voetbal@msn.com>
2020-04-20 20:00:33 +0200

    changed database in config file

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ define("APP_DIR", __DIR__);
 define("HTTP_DIR", "/clean_mvc_template");
 
 define("DB_HOST", "localhost");
-define("DB_DB", "db_naam");
+define("DB_DB", "database");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
